958b081577c06acef1b9946f7f460fc0aaa79b0e
Hristo Vasilev <hristo39@gmail.com>
2024-09-29 19:54:49 +0300

    Added spells for Priest and Mage

--- Spells/mage_spell_handler.py
+++ Spells/mage_spell_handler.py
@@ -1,7 +1,102 @@
+"""
+Handler library, that contains all Mage spells.
+"""
+
+import math
+
+
 # ---------------------------------------------------------------------------- #
-class MageSpells:
+#                                    Classes                                   #
+# ---------------------------------------------------------------------------- #
+class FireMageSpells:
 
     # ------------------------------------------------------------------------ #
-    # TODO: add docstring
-    def __init__(self):
-        pass
+    def __init__(self, hero_instance):
+        self._current_health        = hero_instance._health
+        self._max_health            = hero_instance._health
+        self._current_mana          = hero_instance._secondary_pool
+        self._max_mana              = hero_instance._secondary_pool
+        self._spell_power           = hero_instance._spell_power
+
+    # ------------------------------------------------------------------------ #
+    def cast_fireball(self):
+        """
+        Hurl a fireball to the target, dealing significant fire damage.
+
+        Returns:
+            tuple: damage of the spell, cooldown of the spell
+        """
+        cooldown = 3
+        spell_cost           = math.ceil(self._current_mana * 2 / 100)
+        spell_damage         = math.ceil(self._spell_power  * 155 / 100)
+        self._current_mana  -= spell_cost
+
+        return spell_damage, cooldown
+
+    # ------------------------------------------------------------------------ #
+    def cast_fire_blast(self):
+        """
+        Blast the target with fire, decreasing their damage reduction by 15% for 3 turns.
+
+        Returns:
+            tuple: damage of the spell, amount of damage reduction to be applied,
+            turns for which the effect is active
+        """
+        turns_active         = 3
+        damage_reduction     = 15
+        spell_cost           = math.ceil(self._current_mana * 1 / 100)
+        spell_damage         = math.ceil(self._spell_power  * 82 / 100)
+        self._current_mana  -= spell_cost
+
+        return spell_damage, damage_reduction, turns_active
+
+    # ------------------------------------------------------------------------ #
+    def cast_flamestrike(self):
+        """
+        Ignite the ground under the feet of your target, making them take damage over time for 3 turns.
+        Cooldown - 5 turns.
+
+        Returns:
+            tuple: damage of the spell, cooldown of the spell, turns for which the spell is active
+        """
+        turns_active         = 3
+        cooldown             = 5
+        spell_cost           = math.ceil(self._current_mana * 1 / 100)
+        spell_damage         = math.ceil(self._spell_power  * 57 / 100)
+        self._current_mana  -= spell_cost
+
+        return spell_damage, cooldown, turns_active
+
+    # ------------------------------------------------------------------------ #
+    def cast_polymorph(self):
+        """
+        Pacify the target for 2 turns, making them unable to cast any spells.
+        Any damage suffered by the target will break the effect.
+        Cooldown - 5 turns.
+
+        Returns:
+            tuple: cooldown of the spell, turns for which the spell is active
+        """
+        turns_active         = 2
+        cooldown             = 5
+        spell_cost           = math.ceil(self._current_mana * 1 / 100)
+        self._current_mana  -= spell_cost
+
+        return cooldown, turns_active
+
+    # ------------------------------------------------------------------------ #
+    def cast_arcane_intellect(self):
+        """
+        Your arcane understanding, increases your spell power by 10% for 4 turns.
+        Cooldown - 4 turns.
+
+        Returns:
+            tuple: cooldown of the spell, turns for which the spell is active
+        """
+        turns_active         = 4
+        cooldown             = 4
+        self._spell_power   += math.ceil(self._spell_power * 10 / 100)
+        spell_cost           = math.ceil(self._current_mana * 4 / 100)
+        self._current_mana  -= spell_cost
+
+        return cooldown, turns_active
--- Spells/priest_spell_handler.py
+++ Spells/priest_spell_handler.py
@@ -1,7 +1,113 @@
+"""
+Handler library, which contains all Priest spells
+"""
+
+import math
+
+
 # ---------------------------------------------------------------------------- #
-class PriestSpells:
+#                                    Classes                                   #
+# ---------------------------------------------------------------------------- #
+class ShadowPriestSpells:
 
     # ------------------------------------------------------------------------ #
-    # TODO: add docstring
-    def __init__(self):
-        pass
+    def __init__(self, hero_instance):
+        self._current_health        = hero_instance._health
+        self._max_health            = hero_instance._health
+        self._current_mana          = hero_instance._secondary_pool
+        self._max_mana              = hero_instance._secondary_pool
+        self._spell_power           = hero_instance._spell_power
+
+    # ------------------------------------------------------------------------ #
+    def cast_mind_blast(self):
+        """
+        Blast the mind of the target, dealing shadow damage.
+
+        Returns:
+            tuple: damage of the spell, cooldown of the spell
+        """
+        cooldown = 3
+        spell_cost           = math.ceil(self._current_mana * 4 / 100)
+        spell_damage         = math.ceil(self._spell_power  * 73 / 100)
+        self._current_mana  -= spell_cost
+
+        return spell_damage, cooldown
+
+    # ------------------------------------------------------------------------ #
+    def cast_shadow_word_death(self):
+        """
+        A word of dark binding, dealing moderate shadow damage. If the target is below 20% hp,
+        the damage is increased by 150%. If the target does not die, the caster suffers a backash,
+        equal to 5% of the casters max health.
+
+        Returns:
+            tuple: damage of the spell, cooldown of the spell
+        """
+        cooldown = 3
+        spell_cost           = math.ceil(self._current_mana * 1 / 100)
+        spell_damage         = math.ceil(self._spell_power  * 85 / 100)
+        self._current_mana  -= spell_cost
+
+        return spell_damage, cooldown
+
+    # ------------------------------------------------------------------------ #
+    def cast_devouring_plague(self):
+        """
+        Afflict the target with a disease, dealing initial shadow damage, damage over time
+        and restoring health to the caster.
+
+        Returns:
+            tuple: initial spell damage, damage over time, amount of health restored
+            to the caster, cooldown of the spell, turns for which the spell is active
+        """
+        cooldown             = 4
+        turns_active         = 3
+        spell_cost           = math.ceil(self._current_mana   * 10 / 100)
+        initial_spell_damage = math.ceil(self._spell_power    * 155 / 100)
+        health_leech         = math.ceil(initial_spell_damage * 30 / 100)
+        damage_over_time     = math.ceil(initial_spell_damage * 13 / 100)
+        self._current_mana  -= spell_cost
+
+        return initial_spell_damage, damage_over_time, health_leech, cooldown, turns_active
+
+    # ------------------------------------------------------------------------ #
+    def cast_flash_heal(self):
+        """
+        Quick healing spell, healing for moderate amount.
+        """
+        spell_cost           = math.ceil(self._current_mana * 10 / 100)
+        amount_to_heal       = math.ceil(self._spell_power  * 203 / 100)
+        self._current_mana  -= spell_cost
+        self._heal_up(amount_to_heal)
+
+    # ------------------------------------------------------------------------ #
+    def cast_power_word_shield(self):
+        """
+        Powerfull word that creates a shield around the caster, absorbing a fixed amount of incoming damage.
+
+        Returns:
+            tuple: amount of damage that will be absorbed, cooldown of the spell
+        """
+        cooldown = 5
+        spell_cost           = math.ceil(self._current_mana * 10 / 100)
+        damage_to_absorb     = math.ceil(self._spell_power  * 336 / 100)
+        self._current_mana  -= spell_cost
+
+        return damage_to_absorb, cooldown
+
+    # ------------------------------------------------------------------------ #
+    def _heal_up(self, heal_amount):
+        """
+        Main healing spell logic. Checks the current health of the hero and also the incoming amount.
+        If the incoming amount is overhealing, the current health will be set to the max health.
+
+        Args:
+            heal_amount (int): value of the incoming heal
+        """
+        if self._current_health < 0:
+            self._current_health = 0
+
+        elif self._current_health + heal_amount > self._max_health:
+            self._current_health = self._max_health
+
+    # ------------------------------------------------------------------------ #
