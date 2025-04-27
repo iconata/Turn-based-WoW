d288f69ba4654afe71b2e5f8ff649173740821c2
Hristo Vasilev <hristo39@gmail.com>
2025-04-27 12:43:44 +0300

    Added initial implementation of shaman spells

--- Spells/shaman_spell_handler.py
+++ Spells/shaman_spell_handler.py
@@ -12,20 +12,83 @@ class CommonSpellsMixin:
     This class contains the common spells and abilities of the Shaman.
     """
 
-    def cast_lighting_bolt(self):
-        raise NotImplementedError
+    # ------------------------------------------------------------------------ #
+    @staticmethod
+    def __is_flame_shock_active(active_spells: list) -> bool:
+        if "flame_shock" in active_spells:
+            return True
+
+        return False
 
     # ------------------------------------------------------------------------ #
-    def cast_flame_shock(self):
-        raise NotImplementedError
+    def cast_lighting_bolt(self) -> tuple:
+        """
+        Hurls a bolt of lightning at the target, dealing nature damage.
+
+        Returns:
+            tuple: spell damage, cooldown
+
+        """
+        cooldown = 1
+        spell_cost = math.ceil(self._max_mana * 4 / 100)
+        spell_damage = math.ceil(self._spell_power * 131 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown
 
     # ------------------------------------------------------------------------ #
-    def cast_primordial_wave(self):
-        raise NotImplementedError
+    def cast_flame_shock(self) -> tuple:
+        """
+        Sears the target with fire, causing initial damage and then dealing damage over time.
+
+        Returns:
+            tuple: initial spell damage, damage over time, cooldown, turns active
+
+        """
+        cooldown = 5
+        turns_active = 3
+        spell_cost = math.ceil(self._max_mana * 5 / 100)
+        initial_spell_damage = math.ceil(self._spell_power * 30 / 100)
+        damage_over_time = math.ceil(self._spell_power * 120 / 100)
+        self._secondary_pool -= spell_cost
+
+        return initial_spell_damage, damage_over_time, cooldown, turns_active
 
     # ------------------------------------------------------------------------ #
-    def cast_lava_burst(self):
-        raise NotImplementedError
+    def cast_primordial_wave(self) -> tuple:
+        """
+        Blast the target with a Primordial Wave of energy, dealing elemental damage.
+
+        Returns:
+            tuple: spell damage, cooldown
+
+        """
+        cooldown = 7
+        spell_cost = math.ceil(self._max_mana * 5 / 100)
+        spell_damage = math.ceil(self._spell_power * 525 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown
+
+    # ------------------------------------------------------------------------ #
+    def cast_lava_burst(self, active_spells: list) -> tuple:
+        """
+        Hurls a molten lava ball towards the target, dealing fire damage. Lava Burst will
+        always critically strike if the target is affected by Flame Shock.
+
+        Returns:
+            tuple: spell damage, cooldown
+
+        """
+        cooldown = 2
+        spell_cost = math.ceil(self._max_mana * 5 / 100)
+        spell_damage = math.ceil(self._spell_power * 140 / 100)
+        self._secondary_pool -= spell_cost
+
+        if self.__is_flame_shock_active(active_spells):
+            spell_damage *= 2
+
+        return spell_damage, cooldown
 
 
 # ---------------------------------------------------------------------------- #
@@ -50,20 +113,74 @@ class EnhancementShamanSpells(BaseHeroStats, CommonSpellsMixin):
         self._current_damage_red = self._damage_reduction
 
     # ------------------------------------------------------------------------ #
-    def cast_strormstrike(self):
-        raise NotImplementedError
+    def cast_stormstrike(self) -> tuple:
+        """
+        Energizes both weapons with lightning and delivers a massive blow to your target,
+        dealing Physical damage.
+
+        Returns:
+            tuple: spell damage, cooldown
+
+        """
+
+        cooldown = 2
+        spell_cost = math.ceil(self._max_mana * 5 / 100)
+        spell_damage = math.ceil(self._attack_power * 400 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown
 
     # ------------------------------------------------------------------------ #
-    def cast_lava_lash(self):
-        raise NotImplementedError
+    def cast_lava_lash(self) -> tuple:
+        """
+        Charges your off-hand weapon with lava and burns your target, dealing Fire damage.
+
+        Returns:
+            tuple: spell damage, cooldown
+
+        """
+
+        cooldown = 2
+        spell_cost = math.ceil(self._max_mana * 3 / 100)
+        spell_damage = math.ceil(self._attack_power * 240 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown
 
     # ------------------------------------------------------------------------ #
-    def cast_tempest(self):
-        raise NotImplementedError
+    def cast_tempest(self) -> tuple:
+        """
+        Calls down a tremendous lightning strike, dealing Nature damage to you target.
+
+        Returns:
+            tuple: spell damage, cooldown
+
+        """
+
+        cooldown = 5
+        spell_cost = math.ceil(self._max_mana * 3 / 100)
+        spell_damage = math.ceil(self._spell_power * 310 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown
 
     # ------------------------------------------------------------------------ #
-    def cast_feral_spirit(self):
-        raise NotImplementedError
+    def cast_feral_spirit(self) -> tuple:
+        """
+        Summon two Spirit Wolves to fight by your side for 4 turns.
+
+        Returns:
+            tuple: spell damage, cooldown, turns active
+
+        """
+
+        cooldown = 8
+        turns_active = 4
+        spell_cost = math.ceil(self._max_mana * 5 / 100)
+        spell_damage = math.ceil(self._attack_power * 80 / 100) + math.ceil(self._spell_power * 80 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown, turns_active
 
 
 # ---------------------------------------------------------------------------- #
@@ -88,4 +205,16 @@ class ElementalShamanSpells(BaseHeroStats, CommonSpellsMixin):
 
     # ------------------------------------------------------------------------ #
     def cast_earth_shock(self):
-        raise NotImplementedError
+        """
+        Instantly shocks the target with concussive force, dealing Nature damage.
+
+        Returns:
+            tuple: spell damage, cooldown
+        """
+
+        cooldown = 3
+        spell_cost = math.ceil(self._max_mana * 10 / 100)
+        spell_damage = math.ceil(self._spell_power * 550 / 100)
+        self._secondary_pool -= spell_cost
+
+        return spell_damage, cooldown
